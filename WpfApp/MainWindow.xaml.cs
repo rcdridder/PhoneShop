f65d3511e85d0c9393ab34e3dd652e1f8dab95ba
Rob de Ridder <rderidder@outlook.com>
2023-02-12 13:33:39 +0100

    Wpf App working.

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Domain.Models;
+using Business.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,38 @@ namespace WpfApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        PhoneService phoneService =new();
         public MainWindow()
         {
             InitializeComponent();
+            lbPhones.ItemsSource= phoneService.Sort();
+        }
+
+        private void lbPhones_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            if(lbPhones.SelectedItem != null)
+            {
+                Phone phone = lbPhones.SelectedItem as Phone;
+                tbBrand.Text = phone.Brand;
+                tbType.Text = phone.Type;
+                tbPrice.Text = phone.PriceVat.ToString("C");
+                tbStock.Text = phone.Stock.ToString();
+                tbDescription.Text = phone.Description;
+            }
+        }
+
+        private void tbSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (tbSearch.Text.Length > 0)
+                lbPhones.ItemsSource = phoneService.Search(tbSearch.Text);
+            else
+                lbPhones.ItemsSource = phoneService.Sort();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
