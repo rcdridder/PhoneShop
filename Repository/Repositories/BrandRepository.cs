5db2880b7456c80b6b6d3da458b56d2fd1a93179
Rob de Ridder <rderidder@outlook.com>
2023-02-14 17:10:15 +0100

    Removed unnecessary usings.

--- Repository/Repositories/BrandRepository.cs
+++ Repository/Repositories/BrandRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Business;
 using Business.Domain.Interfaces;
 using Business.Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository.Repositories
 {
